3ee843590a90d709ae9fcde59bc9bbfb9ac52e8a
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-06-14 21:49:16 +0530

    .

--- client/src/components/whatsapp/Whatsapp.jsx
+++ client/src/components/whatsapp/Whatsapp.jsx
@@ -3,7 +3,7 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import "./whatsapp.css";
 const Whatsapp = () => {
   const handleClick = () => {
-    const phoneNumber = "+919064801081 ";
+    const phoneNumber = "+919289615376 ";
 
     const message = encodeURIComponent("Hello! Wroley Scooter");
 
